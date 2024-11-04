9664b15984bcbce6babacfae054127621219a5af
Andrew Hsu <a-hsu@users.noreply.github.com>
2024-11-04 07:13:27 -0500

    feat: Added replication in enemy and playerstate classes along with initializing ASC

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,19 @@
+// Copyright Huck Games
+
+
+#include "Player/AuraPlayerState.h"
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	AttributeSet=CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	
+}
