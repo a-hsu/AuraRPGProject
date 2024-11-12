805412964dd14e3778cf44abcf0f9e47f8fbba3a
Andrew Hsu <a-hsu@users.noreply.github.com>
2024-11-12 05:52:46 -0500

    feat: Create AuraInputConfig

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,5 @@
+// Copyright Huck Games
+
+
+#include "Input/AuraInputConfig.h"
+
---
+++ Source/Aura/Public/Input/AuraInputConfig.h
@@ -0,0 +1,17 @@
+// Copyright Huck Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "AuraInputConfig.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
